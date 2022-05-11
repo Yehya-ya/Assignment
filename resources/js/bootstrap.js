0c0a6fbc9d78d78f1d8345af868048a9eee573dc
Yehya-ya <yahya.yaser@scantitan.com>
2022-05-11 05:47:43 +0300

    add jquery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,11 @@
 window._ = require('lodash');
 
 try {
-    require('bootstrap');
-} catch (e) {}
+    window.bootstrap = require('bootstrap');
+    window.$ = require('jquery');
+} catch (e) {
+    console.log(e);
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
