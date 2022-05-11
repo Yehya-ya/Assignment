27bc87f5c371ee5716614c46f6f1532f4723716d
Yehya-ya <yahya.yaser@scantitan.com>
2022-05-11 05:51:37 +0300

    add a factory for the Category model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,42 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $category = Category::factory()->create(['title' => 'Category A']);
+        for ($i=1;$i<4;$i++) {
+            $subCategory = Category::factory()->create(['category_id' => $category->id, 'title' => 'Sub A-'.$i]);
+            for ($j=1;$j<4;$j++) {
+                $subSubCategory = Category::factory()->create(['category_id' => $subCategory->id, 'title' => 'Sub Sub A-'.$i.'-'.$j]);
+                for ($z=1;$z<4;$z++) {
+                    Category::factory()->create(['category_id' => $subSubCategory->id, 'title' => 'Sub Sub Sub A-'.$i.'-'.$j.'-'.$z]);
+                }
+            }
+        }
+        $category = Category::factory()->create(['title' => 'Category B']);
+        for ($i=1;$i<4;$i++) {
+            $subCategory = Category::factory()->create(['category_id' => $category->id, 'title' => 'Sub B-'.$i]);
+            for ($j=1;$j<4;$j++) {
+                $subSubCategory = Category::factory()->create(['category_id' => $subCategory->id, 'title' => 'Sub Sub B-'.$i.'-'.$j]);
+                for ($z=1;$z<4;$z++) {
+                    Category::factory()->create(['category_id' => $subSubCategory->id, 'title' => 'Sub Sub Sub B-'.$i.'-'.$j.'-'.$z]);
+                }
+            }
+        }
+        $category = Category::factory()->create(['title' => 'Category C']);
+        for ($i=1;$i<4;$i++) {
+            $subCategory = Category::factory()->create(['category_id' => $category->id, 'title' => 'Sub C-'.$i]);
+            for ($j=1;$j<4;$j++) {
+                $subSubCategory = Category::factory()->create(['category_id' => $subCategory->id, 'title' => 'Sub Sub C-'.$i.'-'.$j]);
+                for ($z=1;$z<4;$z++) {
+                    Category::factory()->create(['category_id' => $subSubCategory->id, 'title' => 'Sub Sub Sub C-'.$i.'-'.$j.'-'.$z]);
+                }
+            }
+        }
     }
 }
